584141aa0dfe5d364d3faac34dc3b0dc6ab450a9
Radi Rusadi <radi.susanto@modalrakyat.id>
2019-11-22 14:07:26 +0700

    Initial Commit

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
+import {
+  StyleSheet,
+  Platform,
+  Image,
+  Text,
+  View,
+  ScrollView,
+} from 'react-native';
 
 import firebase from 'react-native-firebase';
 
@@ -13,7 +20,6 @@ export default class App extends React.Component {
     // TODO: You: Do firebase things
     // const { user } = await firebase.auth().signInAnonymously();
     // console.warn('User -> ', user.toJSON());
-
     // await firebase.analytics().logEvent('foo', { bar: '123'});
   }
 
@@ -21,13 +27,14 @@ export default class App extends React.Component {
     return (
       <ScrollView>
         <View style={styles.container}>
-          <Image source={require('./assets/ReactNativeFirebase.png')} style={[styles.logo]}/>
+          <Image
+            source={require('./assets/ReactNativeFirebase.png')}
+            style={[styles.logo]}
+          />
           <Text style={styles.welcome}>
             Welcome to {'\n'} React Native Firebase
           </Text>
-          <Text style={styles.instructions}>
-            To get started, edit App.js
-          </Text>
+          <Text style={styles.instructions}>To get started, edit App.js</Text>
           {Platform.OS === 'ios' ? (
             <Text style={styles.instructions}>
               Press Cmd+R to reload,{'\n'}
@@ -40,21 +47,51 @@ export default class App extends React.Component {
             </Text>
           )}
           <View style={styles.modules}>
-            <Text style={styles.modulesHeader}>The following Firebase modules are pre-installed:</Text>
-            {firebase.admob.nativeModuleExists && <Text style={styles.module}>admob()</Text>}
-            {firebase.analytics.nativeModuleExists && <Text style={styles.module}>analytics()</Text>}
-            {firebase.auth.nativeModuleExists && <Text style={styles.module}>auth()</Text>}
-            {firebase.config.nativeModuleExists && <Text style={styles.module}>config()</Text>}
-            {firebase.crashlytics.nativeModuleExists && <Text style={styles.module}>crashlytics()</Text>}
-            {firebase.database.nativeModuleExists && <Text style={styles.module}>database()</Text>}
-            {firebase.firestore.nativeModuleExists && <Text style={styles.module}>firestore()</Text>}
-            {firebase.functions.nativeModuleExists && <Text style={styles.module}>functions()</Text>}
-            {firebase.iid.nativeModuleExists && <Text style={styles.module}>iid()</Text>}
-            {firebase.links.nativeModuleExists && <Text style={styles.module}>links()</Text>}
-            {firebase.messaging.nativeModuleExists && <Text style={styles.module}>messaging()</Text>}
-            {firebase.notifications.nativeModuleExists && <Text style={styles.module}>notifications()</Text>}
-            {firebase.perf.nativeModuleExists && <Text style={styles.module}>perf()</Text>}
-            {firebase.storage.nativeModuleExists && <Text style={styles.module}>storage()</Text>}
+            <Text style={styles.modulesHeader}>
+              The following Firebase modules are pre-installed:
+            </Text>
+            {firebase.admob.nativeModuleExists && (
+              <Text style={styles.module}>admob()</Text>
+            )}
+            {firebase.analytics.nativeModuleExists && (
+              <Text style={styles.module}>analytics()</Text>
+            )}
+            {firebase.auth.nativeModuleExists && (
+              <Text style={styles.module}>auth()</Text>
+            )}
+            {firebase.config.nativeModuleExists && (
+              <Text style={styles.module}>config()</Text>
+            )}
+            {firebase.crashlytics.nativeModuleExists && (
+              <Text style={styles.module}>crashlytics()</Text>
+            )}
+            {firebase.database.nativeModuleExists && (
+              <Text style={styles.module}>database()</Text>
+            )}
+            {firebase.firestore.nativeModuleExists && (
+              <Text style={styles.module}>firestore()</Text>
+            )}
+            {firebase.functions.nativeModuleExists && (
+              <Text style={styles.module}>functions()</Text>
+            )}
+            {firebase.iid.nativeModuleExists && (
+              <Text style={styles.module}>iid()</Text>
+            )}
+            {firebase.links.nativeModuleExists && (
+              <Text style={styles.module}>links()</Text>
+            )}
+            {firebase.messaging.nativeModuleExists && (
+              <Text style={styles.module}>messaging()</Text>
+            )}
+            {firebase.notifications.nativeModuleExists && (
+              <Text style={styles.module}>notifications()</Text>
+            )}
+            {firebase.perf.nativeModuleExists && (
+              <Text style={styles.module}>perf()</Text>
+            )}
+            {firebase.storage.nativeModuleExists && (
+              <Text style={styles.module}>storage()</Text>
+            )}
           </View>
         </View>
       </ScrollView>
@@ -97,5 +134,5 @@ const styles = StyleSheet.create({
     fontSize: 14,
     marginTop: 4,
     textAlign: 'center',
-  }
+  },
 });
